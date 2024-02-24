078e24759dbd9688e5b0e92066197e04d2f2b084
Glen Danielle D. Galit <glendanielleg@gmail.com>
2024-02-24 22:39:26 +0800

    revert it

--- backEnd/routes/auth.js
+++ backEnd/routes/auth.js
@@ -42,7 +42,7 @@ router.post('/refreshToken', async(req, res)=>{
     })
 })
 
-router.post("/login/success", async(req, res)=>{
+router.get("/login/success", async(req, res)=>{
     const user = await req.user
     try{
         console.log('mcdo', user)
