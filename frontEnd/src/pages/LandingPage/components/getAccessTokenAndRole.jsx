499224d8a4c2840258c679b75d89fa1057bfe192
Julssssssss <gillojulianna12@gmail.com>
2024-03-06 23:29:12 +0800

    update vercel.json

--- frontEnd/src/pages/LandingPage/components/getAccessTokenAndRole.jsx
+++ frontEnd/src/pages/LandingPage/components/getAccessTokenAndRole.jsx
@@ -3,7 +3,7 @@ import { axiosFetchToken } from "../../../components/api/axios";
 const getAccessTokenAndRole = async() => {
     
     try {
-        const response = await axiosFetchToken.post();
+        const response = await axiosFetchToken.get();
         const { accessToken, role, TAC } = response.data;
         localStorage.setItem('accessToken', accessToken)
         localStorage.setItem('role', role)
